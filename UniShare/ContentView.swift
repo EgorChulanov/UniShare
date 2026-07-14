92f9895e01e9c5644556814ddbcecd84804db430
Egor Chulanov <evchulanov@edu.hse.ru>
2026-07-14 16:56:07 +0300

    Initial Commit

---
+++ UniShare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UniShare
+//
+//  Created by Egor Chulanov on 7/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UniShare/UniShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniShareApp.swift
+//  UniShare
+//
+//  Created by Egor Chulanov on 7/14/26.
+//
+
+import SwiftUI
+
+@main
+struct UniShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
